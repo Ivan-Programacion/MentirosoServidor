a0ba60b9a5370c95b58fd84002dc43bf6a013c3c
Iván Vázquez Martín <ivan.vazquez@live.u-tad.com>
2025-12-11 10:08:53 +0100

    Commit inicial

---
+++ src/main/java/com/mentiroso_servidor/MentirosoServidor/MentirosoServidorApplication.java
@@ -0,0 +1,13 @@
+package com.mentiroso_servidor.MentirosoServidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentirosoServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentirosoServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentiroso_servidor/MentirosoServidor/MentirosoServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentiroso_servidor.MentirosoServidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentirosoServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
