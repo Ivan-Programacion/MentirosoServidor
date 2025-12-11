2e81bd67d37d83b6685cfff0e6324d5039777691
Iván Vázquez Martín <ivanvmartin@gmail.com>
2025-12-11 18:55:39 +0100

    Getters and Setters. Añadiendo prueba de conexión.

--- src/main/java/com/mentiroso_servidor/MentirosoServidor/Partida.java
+++ src/main/java/com/mentiroso_servidor/MentirosoServidor/Partida.java
@@ -9,4 +9,50 @@ public class Partida {
 	private int rondas;
 	private HashMap<String, ArrayList<String>[]> baraja;
 	private ArrayList<Jugador> jugadores;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getIdJugadorActual() {
+		return idJugadorActual;
+	}
+
+	public void setIdJugadorActual(int idJugadorActual) {
+		this.idJugadorActual = idJugadorActual;
+	}
+
+	public int getRondas() {
+		return rondas;
+	}
+
+	public void setRondas(int rondas) {
+		this.rondas = rondas;
+	}
+
+	public HashMap<String, ArrayList<String>[]> getBaraja() {
+		return baraja;
+	}
+
+	public void setBaraja(HashMap<String, ArrayList<String>[]> baraja) {
+		this.baraja = baraja;
+	}
+
+	public ArrayList<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(ArrayList<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+
+	@Override
+	public String toString() {
+		return "Partida [id=" + id + ", idJugadorActual=" + idJugadorActual + ", rondas=" + rondas + ", baraja="
+				+ baraja + ", jugadores=" + jugadores + "]";
+	}
 }
